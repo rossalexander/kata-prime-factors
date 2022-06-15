8375dc4305d44ecead47051ed2d1eedc9f8564e5
Ross Alexander <rossaalexander@gmail.com>
2022-06-15 18:42:44 +0200

    Initial commit

---
+++ tests/PrimeFactorsTest.php
@@ -0,0 +1,34 @@
+<?php
+
+
+use App\PrimeFactors;
+use PHPUnit\Framework\TestCase;
+
+class PrimeFactorsTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider factors
+     */
+    public function it_generates_prime_factors_for_1($number, $expected)
+    {
+        $factors = new PrimeFactors();
+        $this->assertEquals($expected, $factors->generate($number));
+    }
+
+    public function factors(): array
+    {
+        return [
+            [1, []],
+            [2, [2]],
+            [3, [3]],
+            [4, [2, 2]],
+            [5, [5]],
+            [6, [2, 3]],
+            [7, [7]],
+            [8, [2, 2, 2]],
+            [100, [2, 2, 5, 5]]
+        ];
+    }
+
+}
